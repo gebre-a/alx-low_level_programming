a2596ac2dd8244a9b13398bcdef899e21d99b715
gebre-a <kidanalemayehu12@gmail.com>
2023-08-10 05:55:55 -0700

    6-sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
-  * main - program to comute various variable
-  * Return: 0 (success)
+ * main - program to comute various variable
+ * Return: 0 (success)
 */
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", siizeof(char));
-	printf("Size of an int: %lu byte(s)\n", siizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", siizeof(long int));
-	printf("Size of a long long int: %lu byte(s)\n", siizeof(long long int));
-	printf("Size of a float: %lu byte(s)\n", siizeof(float));
-	return (0);
+        printf("Size of an int: %lu byte(s)\n", siizeof(int));
+        printf("Size of a long int: %lu byte(s)\n", siizeof(long int));
+        printf("Size of a long long int: %lu byte(s)\n", siizeof(long long int));
+        printf("Size of a float: %lu byte(s)\n", siizeof(float));
+        return (0);
 }
+
