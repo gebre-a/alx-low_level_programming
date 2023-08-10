cb33c5773ba8b52514f0dbd2d7ee26f7608a49ef
gebre-a <kidanalemayehu12@gmail.com>
2023-08-10 02:10:25 -0700

    Write a C program that prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+with proper grammar, but the outcome is a piece of art,#include <stdio.h>
 /**
 * main - A program that prints a line using the printf function
 * Return: 0 (success)
