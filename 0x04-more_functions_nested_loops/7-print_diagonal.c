2fc10234c9236229d098667984ee7712d304f97d
gebre-a <kidanalemayehu12@gmail.com>
2023-08-17 03:15:58 -0700

    ./7-diagonals.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,8 +18,8 @@ void print_diagonal(int n)
 		for (postn = 1; postn <= n; ++postn)
 		{
 			for (space = 1; space <= postn; ++space)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
+				_putchar(32);
+			_putchar(92); 
 			_putchar('\n');
 		}
 	}
