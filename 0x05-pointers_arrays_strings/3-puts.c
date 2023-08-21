f2f7f03e99848b0340268a4ffcdcc8d2f4400d60
gebre-a <kidanalemayehu12@gmail.com>
2023-08-21 04:49:53 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a newline to stdout
+ *
+ * @str: string parameter to print
+ *
+ * Return: Nothing
+*/
+
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+		
+	}
+	_putchar('\n');
+}
