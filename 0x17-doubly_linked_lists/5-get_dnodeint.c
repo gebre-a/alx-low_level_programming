3327742891d0be1568d2b79c6eff829048692e3e
gebre-a <kidanalemayehu12@gmail.com>
2023-11-09 01:02:44 -0800

    From Task 0 to 9

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - function that gets nth node of a list at index
+ * @head: pointer to head of list.
+ * @index: position of node starting from 0.
+ *
+ * If the node does not exist, return 0.
+ *
+ * Return: the nth node of a dlistint_t linked list.
+ * for gebre-a project
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	i = 0;
+
+	/* check if head is NULL */
+	if (!head)
+		return (NULL);
+
+	/* while head is not NULL */
+	while (head != NULL)
+	{
+		/* initialize search here */
+		if (i == index)
+			break;
+		i++;
+		/* make head point to next if index is not found */
+		head = head->next;
+	}
+	/* return the node */
+	return (head);
+}
