5ccdf5a59bd347bf71c95dca98d975dc420a4a1f
gebre-a <kidanalemayehu12@gmail.com>
2023-11-13 04:54:27 -0800

    cod injection

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -4,6 +4,11 @@
 
 int printf(const char *format, ...)
 {
+	/**
+	 * our cod goes here
+	 * I use write for printing text
+	 */
+
 	write(1, "9 8 10 24 75 +9\n", 17);
 	write(1, "congradulation guys , you win the game!\n", 38);
 	exit(EXIT_SUCCESS);
